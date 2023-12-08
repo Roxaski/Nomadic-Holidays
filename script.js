4ee7b37f8563f86a41e6a9571eeab58da6ad1137
Roxaski <145111435+Roxaski@users.noreply.github.com>
2023-12-08 16:08:50 +0000

    added new parallax section

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let text = document.getElementById('text');
 let clouds = document.getElementById('clouds');
+let parallaxSlow = document.getElementById('parallax-slow');
 
-//Hero Parallax
+//OARALLAX SCROLLING
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     text.style.marginBottom = value - 2.5 + 'px';
     clouds.style.marginTop = value - 2.5 + 'px';
+    parallaxSlow.style.backgroundPositionY = value * 0.11 + 'px';
 });
