74879c359345d115590406b84ff13ec21106d14e
Roxaski <145111435+Roxaski@users.noreply.github.com>
2023-12-14 09:43:33 +0000

    new parralax images

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-let text = document.getElementById('text');
-let clouds = document.getElementById('clouds');
 let parallaxSlow = document.getElementById('parallax-slow');
-let pricingParallax = document.getElementById('pricing-parallax');
 
 //PARALLAX SCROLLING
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-
-    text.style.marginBottom = value - 2.5 + 'px';
-    clouds.style.marginTop = value - 2.5 + 'px';
+    
     parallaxSlow.style.backgroundPositionY = value * .25 + 'px';
 });
