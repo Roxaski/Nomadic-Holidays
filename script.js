0373470b408408969f2ae498b6c077fd5750f28d
Roxaski <145111435+Roxaski@users.noreply.github.com>
2023-12-14 12:04:20 +0000

    parralax hero complete

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let test = document.getElementById('test');
+let cloudLeft = document.getElementById('cloud-left');
+let cloudRight = document.getElementById('cloud-right');
+let sun = document.getElementById('sun');
 let parallaxSlow = document.getElementById('parallax-slow');
 
 //PARALLAX SCROLLING
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
     
-    test.style.marginTop = value - .1 + 'px',
+    cloudLeft.style.marginTop = value - .1 + 'px',
+    cloudRight.style.marginTop = value - .1 + 'px',
+    sun.style.marginTop = value - .1 + 'px',
     parallaxSlow.style.backgroundPositionY = value * .25 + 'px';
 });
