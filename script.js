f878951319afed430d8a58a883913c2689834f00
Roxaski <145111435+Roxaski@users.noreply.github.com>
2023-12-14 10:40:31 +0000

    fixed parallax going in front of other content

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let test = document.getElementById('test');
 let parallaxSlow = document.getElementById('parallax-slow');
 
 //PARALLAX SCROLLING
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
     
+    test.style.marginTop = value - .1 + 'px',
     parallaxSlow.style.backgroundPositionY = value * .25 + 'px';
 });
