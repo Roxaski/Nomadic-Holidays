55ebb895308cdb7bdb518b3f20c4cfe2901a4778
Roxaski <145111435+Roxaski@users.noreply.github.com>
2023-12-15 14:53:34 +0000

    vrious improvements

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let cloudLeft = document.getElementById('cloud-left');
 let cloudRight = document.getElementById('cloud-right');
+let cloudLeftMobile = document.getElementById('cloud-left-mobile');
+let cloudRightMobile = document.getElementById('cloud-right-mobile');
 let sun = document.getElementById('sun');
 let parallaxSlow = document.getElementById('parallax-slow');
 let parallaxSlowMobile = document.getElementById('parallax-slow-mobile');
@@ -10,6 +12,8 @@ window.addEventListener('scroll', () => {
     
     cloudLeft.style.marginTop = value - .1 + 'px',
     cloudRight.style.marginTop = value - .1 + 'px',
+    cloudLeftMobile.style.marginTop = value - .1 + 'px',
+    cloudRightMobile.style.marginTop = value - .1 + 'px',
     sun.style.marginTop = value - .1 + 'px',
     parallaxSlow.style.backgroundPositionY = value * .27 + 'px';
     parallaxSlowMobile.style.backgroundPositionY = value * .09 + 'px';
