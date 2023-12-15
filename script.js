ec6a2c2755147cf88af08c1cf21be21f7aa61ab5
Roxaski <145111435+Roxaski@users.noreply.github.com>
2023-12-15 09:12:41 +0000

    potential fix for parallax slow sleep on mobile devices

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let cloudLeft = document.getElementById('cloud-left');
 let cloudRight = document.getElementById('cloud-right');
 let sun = document.getElementById('sun');
 let parallaxSlow = document.getElementById('parallax-slow');
+let parallaxSlowMobile = document.getElementById('parallax-slow-mobile');
 
 //PARALLAX SCROLLING
 window.addEventListener('scroll', () => {
@@ -10,5 +11,6 @@ window.addEventListener('scroll', () => {
     cloudLeft.style.marginTop = value - .1 + 'px',
     cloudRight.style.marginTop = value - .1 + 'px',
     sun.style.marginTop = value - .1 + 'px',
-    parallaxSlow.style.backgroundPositionY = value * .25 + 'px';
+    parallaxSlow.style.backgroundPositionY = value * .27 + 'px';
+    parallaxSlowMobile.style.backgroundPositionY = value * .09 + 'px';
 });
