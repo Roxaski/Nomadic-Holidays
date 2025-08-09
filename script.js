2bc91fb925c707aa735c8f4fed8e9a00c537e871
Roxas Ski <roxaskiGithub@gmail.com>
2025-08-09 16:16:04 +0100

    minor code update, along with more detailed comments

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let leftImg = document.getElementById('left-img');
-let bookNow = document.getElementById('book-now');
+const parallax = document.getElementById('parallax');
+const bookNow = document.getElementById('book-now');
 
-// parallax scrolling
+// 
 window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-    leftImg.style.marginTop = value - .1 + 'px',
-    bookNow.style.backgroundPositionY = value * .27 + 'px';
+    // stores the value of the current vertical scroll position
+    let parallaxScroll = window.scrollY;
+
+    /*
+        multiplying the scroll position by a small number,
+        makes the background image scroll slower than the page, creating a parallax effect
+    */
+    parallax.style.transform = `translateY(${parallaxScroll * .1}px)`;
+
+    /*
+        multiplying the scroll position by a larger number,
+        makes the book-now div move faster than the the background, creating a parallax effect
+    */
+    bookNow.style.backgroundPositionY = parallaxScroll * .27 + 'px';
 });
