6b72b47220b6b3164a8c216c593fd7b822d8e389
Roxas Ski <roxaskiGithub@gmail.com>
2025-09-01 14:44:46 +0100

    various design and performance improvements

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-const parallax = document.getElementById('parallax');
+const hero = document.getElementById('hero');
 const bookNow = document.getElementById('book-now');
 
-// 
+// flag to prevent multiple requestAnimationFrames from stacking up
+let animationScheduled = false;
+
+// function that does the actual parallax animation
+function parallaxAnimation() {
+    // contains the current scroll position
+    const scroll = window.scrollY;
+    
+    // slowly animates the hero parallax element when scrolling
+    hero.style.transform = `translateY(${scroll * 0.1}px)`;
+    
+    // more quickly animates the book now banner when scrolling
+    bookNow.style.backgroundPositionY = scroll * 0.27 + 'px';
+    
+    // when the function runs, it resets this flag ready for the next animation
+    animationScheduled = false;
+};
+
 window.addEventListener('scroll', () => {
-    // stores the value of the current vertical scroll position
-    let parallaxScroll = window.scrollY;
-
-    /*
-        multiplying the scroll position by a small number,
-        makes the background image scroll slower than the page, creating a parallax effect
-    */
-    parallax.style.transform = `translateY(${parallaxScroll * .1}px)`;
-
-    /*
-        multiplying the scroll position by a larger number,
-        makes the book-now div move faster than the the background, creating a parallax effect
-    */
-    bookNow.style.backgroundPositionY = parallaxScroll * .27 + 'px';
+    // only run if we haven't already scheduled an animation
+    if (!animationScheduled) {
+        // schedule the animation for the next frame
+        requestAnimationFrame(parallaxAnimation);
+        
+        // this flag triggers when an animation is scheduled to run and prevents more requestAnimationFrame calls until the animation runs
+        animationScheduled = true;
+    };
 });
